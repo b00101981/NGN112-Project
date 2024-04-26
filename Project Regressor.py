9a26dcc7222669a76292ffc476e1d68540fea139
BuildTools <unconfigured@null.spigotmc.org>
2024-04-26 16:52:43 +0400

    Normalization implemented, but output coefficients still swinging wildly depending on random state. R^2 values are somehow in the negative thousands. MSE similarly absurd.

--- Project Regressor.py
+++ Project Regressor.py
@@ -3,7 +3,7 @@ import seaborn as sns
 import pandas as pd
 import numpy as np
 from pandas.api.types import is_numeric_dtype
-from sklearn.datasets import fetch_california_housing
+from sklearn import preprocessing
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
@@ -17,11 +17,18 @@ print(data.head())
 
 indCols = ['State', 'Coverage', 'Education', 'Emp_Status', 'Gender', 'Income', 'Loc_Code', 'M_Status', 'M_Prem', 'Mo_Claim', 'Mo_Policy', 'N_Complaints', 'N_Policies', 'P_Type', 'S_Channel', 'T_Claims', 'V_Class', 'V_Size']
 depCols = ['CLV']
-X = data[indCols].to_numpy().reshape(-1, len(indCols)) # Uses a random 15% of the data to test.
+X = data[indCols].to_numpy().reshape(-1, len(indCols)) # Reshapes the columns into rows. 
 y = data[depCols].to_numpy()
 
-testSize = 0.3 # Uses a random 15% of the data to test.
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=testSize, random_state = 1) 
+testSize = float(input("Please input the portion (out of 1) of the data to be used for testing (rather than training): ")) # Uses a random % of the data to test.
+randState = int(input("Please input the random state used to split training/testing data: ")) # Uses a random % of the data to test.
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=testSize, random_state=randState) 
+
+normMode = input("Please input the method of normaliztion (Z-Score, Min-Max, or None): ")
+if normMode.__contains__('Z') | normMode.__contains__('stand'):
+ X_train = preprocessing.StandardScaler().fit_transform(X_train)
+elif normMode.__contains__('M'):
+ X_train = preprocessing.MinMaxScaler().fit_transform(X_train)
 
 regressor = LinearRegression().fit(X_train, y_train)
 y_predictions = regressor.predict(X_test)
