550034383588481d21b71a806bf176551c1c60a5
DizardWizard <net.email.username@gmail.com>
2024-05-06 21:40:30 +0400

    Swapped datasets. The new one is way bigger hence slower to train with.

--- Feature Filter.py
+++ Feature Filter.py
@@ -1,17 +1,17 @@
 import pandas as pd
+import datetime
 
-data = pd.read_csv('Customer-Lifetime-Value-Prediction.csv', header=0) # Importing data from csv for speed. 
-catFeats = ['State', 'Coverage', 'Education', 'Emp_Status', 'Gender', 'Loc_Code', 'M_Status', 'P_Type', 'S_Channel', 'V_Class', 'V_Size']
-numFeats = ['Income', 'M_Prem', 'Mo_Claim', 'Mo_Policy', 'N_Complaints', 'N_Policies', 'T_Claims']
+data = pd.read_csv('Melbourne House Prices.csv', header=0) # Importing data from csv for speed. 
+catFeats = ['Suburb', 'Address', 'Street', 'Type', 'Method', 'SellerG', 'Date', 'Regionname', 'CouncilArea']
 
 for c in catFeats:
     data[c] = data[c].astype('category').cat.codes
 corrMatrix = data.corr(method='pearson')
 gFeatDict = {}
 i = 0
-for c in corrMatrix['CLV']:
-    if abs(c) > 0.025:
+for c in corrMatrix['Price']:
+    if abs(c) > 0.1:
         gFeatDict[corrMatrix.index[i]] = c
     i += 1
 
-print(gFeatDict) #Ignore CLV
+print(gFeatDict) #Ignore Price
